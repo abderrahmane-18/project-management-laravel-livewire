9eaa5a9f786aa98ac894143f927bac59ca94122a
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2024-07-15 17:46:02 +0100

    create database and models

---
+++ app/Models/Task.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'project_id',
+        'status'
+    ];
+    public function projects()
+    {
+        return $this->belongsTo(Project::class,'project_id');
+    }
+}
